e0cfbb0be30bb6550c8daf6df9acfe6b104c96c2
Robert Schneider <robmschn@gmail.com>
2019-06-03 09:47:39 -0400

    added support for MACs that are found on the switch itself

--- traceroute.py
+++ traceroute.py
@@ -31,41 +31,72 @@ while True:
         continue
 
 print ('Searching MAC address...')
-#check to see if the MAC is on the switch
+
+# check to see if the MAC is on the distro and grab some variables
 while True:
-    #issue show mac add add USER MAC
-    showMAC =  net_connect.send_command('show mac add add ' +userMAC)
-    #checks to see if we get an output
+    # issue show mac add add userMAC
+    showMAC = net_connect.send_command('show mac add add ' +userMAC)
+    # checks to see if we get correct output
     if 'Unicast Entries' in showMAC:
-        #splits output into strings
+        # splits output into strings
         MAClst = [];
         for char in showMAC:
             MAClst.append(char)
         MACvarsplit = (''.join(MAClst).split('\n'))
 
-        #grabs only the part of output that contains VLAN
         MACint = MACvarsplit[3]
 
-        #grabs VLAN number
+        # grabs VLAN
         switchVLAN = MACint.split()[0]
+
+        # grabs current interface
+        currentSwitchInt = MACint.split()[4]
+
         break
     else:
-        #if no results, tell the user MAC not found, check MAC
+        # if no results, tell the user MAC not found, check MAC
         print ('\n*****ERROR: MAC NOT FOUND****\n')
-        #offer for them to change MAC and try again or change MAC
-        userMAC = input("\nPlease try again. MAC must be HHHH.HHHH.HHHH format: ")
+        # offer for them to change MAC and try again or change MAC
+        userMAC = input('\nMust be HHHH.HHHH.HHHH format. Please try again: ')
         continue
 
+# runs traceroute and if the MAC is on another switch, it will connect. If the MAC is on the switch itself, it'll go directly to change VLAN
 print('Running traceroute. This may take up one minute...')
-
 while True:
+
     tracerouteMAC = net_connect.send_command('traceroute mac ' +userMAC+ ' ' + userMAC)
+    # MAC is on another switch
     if 'Layer 2 trace completed' in tracerouteMAC:
-        break
+        # makes output into seperate strings
+        TRACElst = [];
+        for char in tracerouteMAC:
+            TRACElst.append(char)
+        TRACEvarsplit = (''.join(TRACElst).split('\n'))
 
-    else:
-        #there is a phone in the middle of the switch and device
+        # grabs only the part of the output that contains IP and interface of MAC
+        TRACEint = TRACEvarsplit[1]
 
+        # grabs switch name
+        switchName = TRACEint.split()[1]
+
+        # grabs switch interface
+        switchInt = TRACEint.split()[-1]
+
+        # grabs switch IP
+        outputSwitchIP = TRACEint.split()[2]
+        switchIP = outputSwitchIP.strip(string.punctuation) # removes () from output
+
+        # tell the user MAC has been found and where it is
+        print ('\nMAC ' +userMAC+ ' has been found! \n\nSwitch: ' +switchName+ ' (' +switchIP+ ')\nInterface: ' +switchInt+ '\nVLAN: ' +switchVLAN)
+
+    # MAC is on current switch.
+    elif 'Source and Destination on same port and no nbr!' in tracerouteMAC:
+	    # tell the user the MAC has been found and is on the current switch
+        print ('\nMAC ' +userMAC+ ' is on this switch! \n\nInterface: ' +currentSwitchInt+ '\nVLAN: ' +switchVLAN)
+        switchInt = currentSwitchInt
+
+    #there is a phone in the middle of the switch and device
+    elif 'Unable to send a l2trace request' in tracerouteMAC:
         #grab phoneIP from output
         PHONElst = [];
         for char in tracerouteMAC:
@@ -88,26 +119,25 @@ while True:
 
         #issue traceroute mac phoneMAC phoneMAC
         tracerouteMAC = net_connect.send_command('traceroute mac ' + phoneMAC + ' ' + phoneMAC)
-        break
 
-#makes output into seperate strings
-TRACElst = [];
-for char in tracerouteMAC:
-    TRACElst.append(char)
-TRACEvarsplit = (''.join(TRACElst).split('\n'))
+        # makes output into seperate strings
+        TRACElst = [];
+        for char in tracerouteMAC:
+            TRACElst.append(char)
+        TRACEvarsplit = (''.join(TRACElst).split('\n'))
 
-#grabs only the part of the output that contains IP and interface of MAC
-TRACEint = TRACEvarsplit[1]
+        # grabs only the part of the output that contains IP and interface of MAC
+        TRACEint = TRACEvarsplit[1]
 
-#grabs switch name
-switchName = TRACEint.split()[1]
+        # grabs switch name
+        switchName = TRACEint.split()[1]
 
-#grabs switch interface
-switchInt = TRACEint.split()[-1]
+        # grabs switch interface
+        switchInt = TRACEint.split()[-1]
 
-#grabs switch IP
-outputSwitchIP = TRACEint.split()[2]
-switchIP = outputSwitchIP.strip(string.punctuation) #removes () from output
+        # grabs switch IP
+        outputSwitchIP = TRACEint.split()[2]
+        switchIP = outputSwitchIP.strip(string.punctuation) # removes () from output
 
-#make everything look pretty
-print ("\nMAC "+userMAC+" has been found!\n\nSwitch: " +switchName+ " (" +switchIP+ ")" "\nInterface: " +switchInt+ "\nVLAN: " +switchVLAN+ "\n")
+        # tell the user MAC has been found and where it is
+        print ('\nMAC ' +userMAC+ ' has been found! \n\nSwitch: ' +switchName+ ' (' +switchIP+ ')\nInterface: ' +switchInt+ '\nVLAN: ' +switchVLAN)
